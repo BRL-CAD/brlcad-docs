6c5611e15865ae8e3a4f3e72dccd65ab85f73945
Dashamir Hoxha <dashohoxha@gmail.com>
2021-06-14 19:34:36 +0200

    Try to customize the UI (#16)

---
+++ utils/build-custom-ui.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+# get antora-ui-default
+git clone https://gitlab.com/antora/antora-ui-default ui-default
+
+# apply customizations
+cp -a ui/src/css/custom.css ui-default/src/css/
+echo '@import "custom.css";' >> ui-default/src/css/site.css
+
+# build the ui bundle
+cd ui-default/
+npm install
+gulp bundle
+cd ..
+
+# move the bundle to ui/
+if [[ -f ui-default/build/ui-bundle.zip ]]; then
+  rm -f ui/ui-bundle.zip
+  mv ui-default/build/ui-bundle.zip ui/
+fi
+
+# cleanup
+rm -rf ui-default/
