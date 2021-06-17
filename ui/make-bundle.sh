141ee5a93ba7466511cc83c2c82205bcb676e320
Dashamir Hoxha <dashohoxha@gmail.com>
2021-06-17 08:30:54 +0200

    Improve the custom CSS styles (#16)

--- ui/make-bundle.sh
+++ ui/make-bundle.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -x
 
+cd $(dirname $0)
+
 # get antora-ui-default
 git clone https://gitlab.com/antora/antora-ui-default ui-default
 
 # apply customizations
-cp -a ui/src/css/custom.css ui-default/src/css/
+cp -a src/css/custom.css ui-default/src/css/
 echo '@import "custom.css";' >> ui-default/src/css/site.css
 
 # build the ui bundle
@@ -16,8 +18,8 @@ cd ..
 
 # move the bundle to ui/
 if [[ -f ui-default/build/ui-bundle.zip ]]; then
-  rm -f ui/ui-bundle.zip
-  mv ui-default/build/ui-bundle.zip ui/
+  rm -f ui-bundle.zip
+  mv ui-default/build/ui-bundle.zip .
 fi
 
 # cleanup
